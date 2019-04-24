8c9b29df74a4e02ab4128f5f48676c74d28115cb
Peter <peter.helsen@hi3.be>
2019-04-24 14:50:27 +0200

    adding build deps to repo

---
+++ build/readme.js
@@ -0,0 +1,63 @@
+const f = require("./../funcl.js");
+const fs = require('fs');
+
+
+const {getIn, array_p, assoc, atom_p, boolean_p, clone, coll_p, concat, count, countable_p, date_p, dec, drop, eq, even_p, filter, first, function_p, inc, last, lowerCase, map, mapEntries_2map, map_p, map_2mapEntries, neg_p, nth, number_p, odd_p, partial, partialR, pipe, pos_p, range, reduce, reverse, regexp_p, rest, string_p, sqr, takeLast, takeWhile, type, undefined_p, upperCase, zero_p} = f; 
+
+
+
+const r = [];
+let arr; let dict; let dict2;
+r.push ("range(10);");
+r.push("arr=range(1,11); arr");
+r.push("count(arr)"); 
+r.push("reverse(arr)");
+r.push("count('λλ FUNCL! λλ') ");
+r.push("reverse('λλ FUNCL! λλ' )");
+r.push("[first(arr), last(arr)]");
+r.push("rest(arr)");
+r.push("takeWhile(x => x < 5, arr)");
+r.push("takeLast(3, arr)");
+r.push("map(sqr,arr)");
+r.push("pipe(arr,map(sqr),reverse,map(x=>x+100),filter(odd_p))");
+r.push("dict=assoc({}, 'fn' , 'Bar' , 'ln', 'Foo')")
+r.push("map(type, [arr,dict,11,true,{},[],new Date(),/funcl/])")
+r.push("dict=pipe(dict,assoc('ln', 'StillFoo', 'address',{ street : 'FunclStreet' , nb :  '12' }));");
+r.push("dict2=clone(dict)");
+r.push("eq(dict,dict2)");
+r.push("dict2=assoc(dict2,'nickname','funky' )")
+r.push("eq(dict,dict2)");
+
+let fileText = `# FunCl
+[![LICENSE MIT](https://img.shields.io/npm/l/funcl.svg)](https://www.npmjs.com/package/funcl)
+
+
+Some javascript util functions that sure as hell ring a clojure bell!
+
+## Get it 
+
+You can install the latest release via npm:
+
+\`\`\`shell
+npm install funcl
+\`\`\`
+
+## Test it 
+
+\`\`\`shell
+npm run test
+\`\`\`
+
+## Do it (some quick examples)
+
+`
+
+fileText += "|||\n";
+fileText += "|--- |--- |\n"; 
+
+const rows =  map(x => `|${x}|${JSON.stringify(eval(x))}|`,r).join("\n");
+fileText +=   rows;
+console.log(fileText);
+fs.writeFile("README.md",fileText, (x,err) => console.log(err))
+
+
